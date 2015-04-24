20047b056809f5c491e78a01bcf05547393d699e
Lan Yang <ireneyanglan@gmail.com>
2015-04-23 23:12:56 -0400

    m

--- apriori.py
+++ apriori.py
@@ -106,16 +106,28 @@ def Apriori(file_name, min_sup, min_conf):
 def Print(freq_set, rules, min_sup, min_conf):
 	outfile = open("output.txt", 'w')
 	# Write frequent itemsets
-	outfile.write('==Frequent itemsets (min_sup= %d%)\n' % (int(min_sup * 100)))
+	# print '==Frequent itemsets (min_sup= {}%)\n'.format(min_sup)
+	outfile.write('==Frequent itemsets (min_sup={}%)\n'.format(int(min_sup * 100)))
 	for item_set,v in freq_set.iteritems():
 		item_set = list(item_set)
 		outfile.write('[')
 		for i in item_set[:-1]:
 			outfile.write('%s,' % i)
 		outfile.write('%s' % item_set[-1])
-		outfile.write('], %d%\n' % (int(v * 100)))
+		outfile.write('], {}%\n'.format(int(v * 100)))
 	# Write high-confidence association rules
-
+	outfile.write('\n==High-confidence association rules (min_conf={}%)\n'.format(int(min_conf * 100)))
+	for k1,v1 in rules.iteritems():
+		k1 = list(k1)
+		for k2,v2 in v1.iteritems():
+			rhs = k2
+			conf = v2[0]
+			supp = v2[1]
+			outfile.write('[')
+			for i in k1[:-1]:
+				outfile.write('%s,' % i)
+			outfile.write('%s' % i)
+			outfile.write('] => [%s] (Conf: {}%, Supp: {}%)\n'.format(rhs, int(conf * 100), int(supp * 100)))
 	outfile.close()
 
 def main():
